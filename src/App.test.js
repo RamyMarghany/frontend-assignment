49ccb895f11cd5e869243851fdb11f788360eb2d
ramy.med7t@gmail.com <ramy.med7t@gmail.com>
2019-04-10 02:26:28 +0200

    made test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, configure } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import SignInForm from './components/SignInForm';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+// test cases for Header
+describe('<Header/>', () =>{
+  const wrapper = shallow(<Header/>);
+  it('should be Logo to be exist',() =>{
+    expect(wrapper.find('.header__logo').exists()).toBe(true);
+  })
+
+ it('should be search-box to disappear firstly',() =>{
+  expect(wrapper.find('.header__input').exists()).toBe(false);
+  })
+
+})
+
+
+// test cases for footer
+describe('<Footer/>', () =>{
+  const wrapper = shallow(<Footer/>);
+  it('should be three footer blocks',() =>{
+    expect(wrapper.find('.footer').length).toBe(3);
+  })
+
+  it('should be item image to be exist',() =>{
+    expect(wrapper.find('.footer__img').exists()).toBe(true);
+  })
+
+  it('should be item image to be exist',() =>{
+    expect(wrapper.find('.footer__name').exists()).toBe(true);
+  })
+
+  it('should be item image to be exist',() =>{
+    expect(wrapper.find('.footer__price').exists()).toBe(true);
+  })
+
+})
+
+
+// test cases for sign in form
+describe('<SignInForm>',()=>{
+  it('matches the snapshot',()=>{
+    const tree= shallow(<Footer/>);
+    expect(toJson(tree)).toMatchSnapshot();
+  })
+})
--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -32,44 +32,44 @@ class SignInForm extends Component {
           <Form onSubmit={this.handleSubmit} className="login-form">
             <h2 className="login-form__title"> sign in now</h2>
             <p className="login-form__description">Unlock awesome features!</p>
-          <Form.Item>
-            {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Required field!' }],
-            })(
-              <Input  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" className="login-form__input" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Required field!' }],
-            })(
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" className="login-form__input" />
-            )}
-          </Form.Item>
-          <Form.Item>
-          <Row>
-            <Col sm={{ span: 12}} xs={{span:24}} className="align-left">
-              {getFieldDecorator('remember', {
-                valuePropName: 'checked',
-                initialValue: true,
+            <Form.Item>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Required field!' }],
               })(
-                <Checkbox>Remember me</Checkbox>
+                <Input  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" className="login-form__input" />
               )}
-            </Col>
-            <Col sm={{ span: 12}} xs={{span:24}} className="align-right">
-              <a className="login-form__forgot">Forgot password</a>
-            </Col>
-            </Row>
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Required field!' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" className="login-form__input" />
+              )}
+            </Form.Item>
+            <Form.Item>
             <Row>
-            <Col xs={{ span: 24}}>
-              <Button type="primary" htmlType="submit" className="login-form__submit">
-                sign in
-              </Button>
-            </Col>
-            </Row>
-          </Form.Item>
-         </Form>
-      </div>
+              <Col sm={{ span: 12}} xs={{span:24}} className="align-left">
+                {getFieldDecorator('remember', {
+                  valuePropName: 'checked',
+                  initialValue: true,
+                })(
+                  <Checkbox>Remember me</Checkbox>
+                )}
+              </Col>
+              <Col sm={{ span: 12}} xs={{span:24}} className="align-right">
+                <a className="login-form__forgot">Forgot password</a>
+              </Col>
+              </Row>
+              <Row>
+              <Col xs={{ span: 24}}>
+                <Button type="primary" htmlType="submit" className="login-form__submit">
+                  sign in
+                </Button>
+              </Col>
+              </Row>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     );
   }
